6958b1e80a6f39d16a7696e8d1d10fafbf650606
Snorre <snorrestrand@hotmail.com>
2024-08-14 11:16:33 +0200

    feat: upgrade to bun

--- constant.ts
+++ constant.ts
@@ -13,7 +13,7 @@ export const mapIpAddress = '8100';
 export const firebasePublicConfig = {
   apiKey: '',
   authDomain: `${projectId}.firebaseapp.com`,
-  projectId: `${projectId}`,
+  projectId,
   storageBucket: `${projectId}.appspot.com`,
   messagingSenderId: '',
   appId: '',
